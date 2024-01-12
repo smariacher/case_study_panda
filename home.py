1c67208b1304b603950178791995350b321dd74b
Julian Fritzer <Fritzerjulian@posteo.de>
2024-01-12 10:12:29 +0100

    changed all

--- home.py
+++ home.py
@@ -1,4 +1,14 @@
-import streamlit as st
+
+import streamlit as st # Variante der Importierung wo immer .st vor den befehlen geschrieben werden muss.
+from tinydb import TinyDB, Query # Variante der Importierung wo nichts mehr vor die Befehle geschrieben werden muss.
+
+    
+db_devices = TinyDB('./data/devices.json')
+db_users = TinyDB('./data/users.json')
+db_reservations = TinyDB('./data/reservations.json')
+ 
+
+
 
 st.set_page_config(
     page_title="Home",
--- pages/Device_Management.py
+++ pages/Device_Management.py
@@ -1,11 +1,13 @@
 import streamlit as st
+from home import db_devices, db_users, db_reservations
+
 
 st.set_page_config(page_title="Device Management")
 
 st.write("# Device Management")
 
 st.write("### Create new device")
-username = st.text_input("Device Name")
+device_name = st.text_input("Device Name")
 device_type = st.text_input("Device Type")
 device_location = st.text_input("Device Location")
 cost_per_quarter = st.number_input("Cost per quarter in €", step=0.01)
@@ -13,13 +15,14 @@ cost_per_quarter = st.number_input("Cost per quarter in €", step=0.01)
 device_maintainer = st.selectbox("Select Device Maintainer", ["Peter", "Hans", "Max"])
 
 if st.button("Add new Device") == True:
-    print(username, device_type, device_location, cost_per_quarter, device_maintainer)
+    db_devices.insert({'device_name': device_name})
+    print(device_name, device_type, device_location, cost_per_quarter, device_maintainer)
     st.success("Device created successfully")
 
 
 st.write("### Change device")
 
-username = st.selectbox("Change Device Name", ["Device 1", "Device 2", "Device 3"])
+device_name = st.selectbox("Change Device Name", ["Device 1", "Device 2", "Device 3"])
 device_type = st.text_input("Change Device Type")
 device_location = st.text_input("Change Device Location")
 cost_per_quarter = st.number_input("Change cost per quarter in €", step=0.01)
@@ -27,5 +30,5 @@ cost_per_quarter = st.number_input("Change cost per quarter in €", step=0.01)
 device_maintainer = st.selectbox("Change Device Maintainer", ["Peter", "Hans", "Max"])
 
 if st.button("Change Device") == True:
-    print(username, device_type, device_location, cost_per_quarter, device_maintainer)
+    print(device_name, device_type, device_location, cost_per_quarter, device_maintainer)
     st.success("Device changed successfully")
