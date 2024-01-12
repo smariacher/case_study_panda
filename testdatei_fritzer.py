052a2aeacd0e326e5d0399956132397e1cc9cfd9
Julian Fritzer <Fritzerjulian@posteo.de>
2024-01-12 11:52:05 +0100

    edited .gitignore and device_managment.py

---
+++ testdatei_fritzer.py
@@ -0,0 +1,15 @@
+
+
+
+from tinydb import TinyDB, Query # Variante der Importierung wo nichts mehr vor die Befehle geschrieben werden muss.
+
+db_devices = TinyDB('./data/devices.json')
+db_users = TinyDB('./data/users.json')
+db_reservations = TinyDB('./data/reservations.json')
+
+query = Query()
+
+abc = db_users.search(query.is_device_mainter == True)
+
+print(abc)
+
